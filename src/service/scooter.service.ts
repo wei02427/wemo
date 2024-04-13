dff3aa6385de491de504e5df2e2dc45de934e165
劉政威 <wei@liuzhengweideMacBook-Pro.local>
2024-04-13 15:49:17 +0800

    Revert "Revert "test""
    This reverts commit d83bfd9401ebcc0ecf1c68a432dd4c9487f746c1.

--- src/service/scooter.service.ts
+++ src/service/scooter.service.ts
@@ -4,17 +4,20 @@ import { ScooterDao } from '../dao/scooter.dao'
 import { Mutex } from 'async-mutex'
 import * as dayjs from 'dayjs'
 import { UserDao } from '../dao/user.dao'
+import { User } from '../interceptor/user'
+
 
 @Injectable()
 export class ScooterService {
   private readonly locks = new Map<number, Mutex>()
-  constructor (
+  constructor(
     private readonly rentDao: RentDao,
     private readonly scooterDao: ScooterDao,
-    private readonly userDao: UserDao
+    private readonly userDao: UserDao,
+    private readonly user: User,
   ) { }
 
-  async rent (userId: number, scooterId: number) {
+  async rent(userId: number, scooterId: number) {
     if (!this.locks.has(scooterId)) this.locks.set(scooterId, new Mutex())
     const mutex = this.locks.get(scooterId)
     const release = await mutex.acquire()
@@ -36,13 +39,13 @@ export class ScooterService {
     }
   }
 
-  async return (rentId: number) {
+  async return(rentId: number) {
     const rentRecord = await this.rentDao.get(rentId)
 
     if (!rentRecord) throw new Error('Rent record not found.')
     if (rentRecord.EndTime) throw new Error('Scooter has been returned.')
-
-    const { ScooterId: scooterId, UserId } = rentRecord
+    if (rentRecord.UserId !== this.user.Id) throw new Error('Not return by same user.')
+      const { ScooterId: scooterId, UserId } = rentRecord
     if (!this.locks.has(scooterId)) this.locks.set(scooterId, new Mutex())
     const mutex = this.locks.get(scooterId)
     const release = await mutex.acquire()
@@ -64,11 +67,11 @@ export class ScooterService {
     }
   }
 
-  async getAvailableList () {
+  async getAvailableList() {
     return await this.scooterDao.getAvailableList()
   }
 
-  async getRentRecord (scooterId: number) {
+  async getRentRecord(scooterId: number) {
     return await this.rentDao.getByScooter(scooterId)
   }
 }
