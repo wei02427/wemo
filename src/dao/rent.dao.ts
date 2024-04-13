aa993e099b9874823a933009321259e6b2eea62c
劉政威 <wei@liuzhengweideMacBook-Pro.local>
2024-04-13 15:46:50 +0800

    test

--- src/dao/rent.dao.ts
+++ src/dao/rent.dao.ts
@@ -4,11 +4,11 @@ import { Injectable } from '@nestjs/common'
 
 @Injectable()
 export class RentDao extends BaseDao<'Rent'> {
-  constructor () {
+  constructor() {
     super('Rent')
   }
 
-  async add (data: Pick<Rent, 'UserId' | 'ScooterId' | 'StartTime'>) {
+  async add(data: Pick<Rent, 'UserId' | 'ScooterId' | 'StartTime'>) {
     return await this.repository
       .insert({
         StartTime: data.StartTime,
@@ -19,7 +19,7 @@ export class RentDao extends BaseDao<'Rent'> {
       .catch((err) => { throw new Error(err) })
   }
 
-  async update (data: Pick<Rent, 'Id'> & Partial<Rent>) {
+  async update(data: Pick<Rent, 'Id'> & Partial<Rent>) {
     const { Id, ...udpateData } = data
     return await this.repository
       .update(udpateData)
@@ -27,7 +27,7 @@ export class RentDao extends BaseDao<'Rent'> {
       .catch((err) => { throw new Error(err) })
   }
 
-  async get (rentId: number) {
+  async get(rentId: number) {
     return await this.repository
       .select('ScooterId')
       .select('UserId')
@@ -37,7 +37,7 @@ export class RentDao extends BaseDao<'Rent'> {
       .catch((err) => { throw new Error(err) })
   }
 
-  async getByScooter (scooterId: number) {
+  async getByScooter(scooterId: number) {
     return await this.repository
       .select('StartTime')
       .select('EndTime')
@@ -45,11 +45,13 @@ export class RentDao extends BaseDao<'Rent'> {
       .where('ScooterId', scooterId)
   }
 
-  async getByUser (userId: number) {
+  async getByUser(userId: number) {
     return await this.repository
       .innerJoin('Scooter', 'Scooter.Id', 'Rent.ScooterId')
       .select('StartTime')
       .select('EndTime')
+      .select('ScooterId')
+      .select({ RentId: 'Rent.Id' })
       .select('Rent.UserId')
       .select('LicensePlate')
       .where('Rent.UserId', userId)
